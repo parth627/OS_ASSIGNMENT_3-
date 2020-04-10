a2614af98160ad86851242edc85a67577fbbe884
Devansh Tadhani <62784304+devansh-tadhani@users.noreply.github.com>
2020-04-10 09:47:20 +0530

    Shortest Remaining Time First
    Sir, My input set generation is in code "input set generator.c". Output of that file is "input.txt".
    This input.txt is again input for my main code file i.e.:"Shortest Remaining Time First.cpp". 
    Output are 3 files:
    1)output.txt
    2)output.csv(excel file)
    3)chart.csv(excel-for simplicity)
    Graph of my processes is made explicitly in graph.pdf

---
+++ input set generator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+	int p,i,a,b,c;
+	FILE *fptr;
+	fptr=fopen("input.txt","w");
+	for(i=1;i<51;i++)
+	{
+		fprintf(fptr,"%d",i);
+		fputs("\nProcess--Arrival--Brust\n",fptr);
+		for(p=1;p<=5;p++)
+		{
+			a=rand()%10+1;
+			b=rand()%10+1;
+			c=rand()%10+1;
+			fprintf(fptr,"%d\t",a);
+			fprintf(fptr,"%d\t",b);
+			fprintf(fptr,"%d\n",c);
+		}
+	}
+	fclose(fptr);
+	return 0;
+}
